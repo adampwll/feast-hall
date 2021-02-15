3e568065d33fb41242d6acd924275d107a3ab1d9
adampwll <adampwll96@gmail.com>
2021-02-15 15:44:43 -0500

    Added Profile component. Some cleanup with main page display.

--- src/components/HomePage.js
+++ src/components/HomePage.js
@@ -1,35 +1,28 @@
-import React from 'react';
+import React, {useState} from 'react';
 import { Col, Container, Row } from 'react-bootstrap';
-import Clock from './Clock';
-import UserProfile from './UserProfile';
+import Profile from './Profile'
 
-class HomePage extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      date: new Date() 
-    }
+function HomePage() {
+  const [content, setContent] = useState(Profile);
+
+  function switchContent(event) {
+    setContent(event.target.value);
   }
 
-  render() {
-    return(
-      <div>
-        <Clock/>
-        <Container>
-          <Row>
-            <Col><h2>Upcoming Games</h2></Col>
-            <Col><h2>Profile</h2></Col>
-            <Col><h2>Players</h2></Col>
-          </Row>
-          <Row>
-            <Col></Col>
-            <Col></Col>
-            <Col><UserProfile userName='Adam'/></Col>
-          </Row>
-        </Container>
-      </div>
-    );
-  }
+  return(
+    <div>
+      <Container>
+        <Row>
+          <Col><h2>Upcoming Games</h2></Col>
+          <Col><h2>Profile</h2></Col>
+          <Col><h2>Players</h2></Col>
+        </Row>
+      </Container>
+      <Container>
+        {content}
+      </Container>
+    </div>
+  );
 }
 
 export default HomePage;
